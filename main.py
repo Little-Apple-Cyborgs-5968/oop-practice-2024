e960c26948f1d99b39a0b291b3a3df1ff22dc8e2
ChristoferAponte <chrisalanaponte25@gmail.com>
2024-09-09 19:06:42 -0500

    we did shlopr

--- main.py
+++ main.py
@@ -1,17 +1,7 @@
 #imports example robot
 from example import ExampleBot
+from myrobo import Myrobo
 
 if __name__ == "__main__":
-
-    #Testing Fred
-    fred = ExampleBot('Fred')
-    fred.speak()
-    fred.on()
-    fred.speak()
-    fred.calculateSquare(4)
-    fred.off()
-    fred.speak()
-    fred.on()
-    fred.calculateSquare(3452324)
-
-
+    bob = Myrobo("myrob")
+    bob.baller(" schlorp")
